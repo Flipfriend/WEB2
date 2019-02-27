41bd6f46c1eeb6b6f22963fc2f8d07d2cc0ab013
Andrew Hill <ahill7.ah@gmail.com>
2019-02-26 16:02:15 -0800

    changed the amount of pxs before button is visible

--- Class 2/main.js
+++ Class 2/main.js
@@ -3,7 +3,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
     document.getElementById("topbutton").style.display = "block";
   } else {
     document.getElementById("topbutton").style.display = "none";
