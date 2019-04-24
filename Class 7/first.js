7054c4faa61494aab134ac993b145198a56eceaa
Andrew Hill <ahill7.ah@gmail.com>
2019-04-23 21:36:51 -0700

    class example push with comments to disable fully written function

---
+++ Class 7/first.js
@@ -0,0 +1,12 @@
+// ******First Example
+alert("IT'S ON FIRE !!!!!!");
+
+var a=6;
+
+if (a%2===1){
+    alert("ODD!!!");
+}
+else{
+    alert("EVEN!!!!");
+}
+
--- Class 7/main.js
+++ Class 7/main.js
@@ -1,11 +1,34 @@
-alert("IT'S ON FIRE !!!!!!");
+// var numberSpan = document.getElementById("numberBox");
+// var randomNumber = Math.random()*2;
+// numberSpan.innerText = randomNumber;
 
-var a=6;
 
-if (a%2===1){
-    alert("ODD!!!");
-}
-else{
-    alert("EVEN!!!!");
-}
+// // var redColor = Math.random()*255;
+// // var greenColor = Math.random()*255;
+// // var blueColor = Math.random()*255;
+
+// // var textColor = "rgb("+redColor+","+greenColor+","+blueColor+")";
+
+
+// // var subHeader = numberSpan.parentElement;
+// // // subHeader.style.color = textColor;
+
+// // body.style.backgroundColor = textColor;
+
+// var myButton = document.getElementById("triggerButton");
+
+// myButton.addEventListener("click",function(){
+//     var redColor = Math.random()*255;
+//     var greenColor = Math.random()*255;
+//     var blueColor = Math.random()*255;
+
+//     var textColor = "rgb("+redColor+","+greenColor+","+blueColor+")";
+
+//     var body = document.getElementById("mainBody");
+
+//     body.style.backgroundColor = textColor;
+
+// })
+
+
     
