5fb7c4e5702362025eec5598d7f2e3573bc95bce
Andrew Hill <ahill7.ah@gmail.com>
2019-04-29 22:01:51 -0700

    added another img progressed on the javascript still figuring out how to make it change

--- Class 10/main.js
+++ Class 10/main.js
@@ -1,1 +1,10 @@
-document.getElementById(kittyImg);
+var imgChange = function(){
+    var img = src="kittenImg";
+}
+
+
+var myButton = getElementById("mainButton");
+
+myButton.addEventListener("click",imgChange);
+
+imgChange();
