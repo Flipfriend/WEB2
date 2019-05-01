d6fdc8640527a085054749efcd5e9e57d5be549a
Andrew Hill <ahill7.ah@gmail.com>
2019-04-30 18:12:40 -0700

    added images renamed kitten to kitten0 and changed fixed the js to try to reflect my function

--- Class 10/main.js
+++ Class 10/main.js
@@ -1,10 +1,13 @@
-var imgChange = function(){
-    var img = src="kittenImg";
+
+var imagesArray = ["images/kitten0.jpg","images/kitten1.jpg","images/kitten2.jpg","images/kitten3.jpg","images/kitten4.jpg"]
+
+function displayImage(){
+    var num = Math.floor(Math.random()*5);
+    document.images.src = imagesArray[num];
 }
 
+var myButton = document.getElementById("mainButton");
 
-var myButton = getElementById("mainButton");
+myButton.addEventListener("onClick",displayImage);
 
-myButton.addEventListener("click",imgChange);
-
-imgChange();
+displayImage();
