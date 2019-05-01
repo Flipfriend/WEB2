521681a6b58c2b87a5f44344286fa029ade9e618
Andrew Hill <ahill7.ah@gmail.com>
2019-04-30 20:21:15 -0700

    changed the functionalirty and got it to work

--- Class 10/main.js
+++ Class 10/main.js
@@ -1,13 +1,17 @@
+var srcImg = document.getElementById('kittyImg2');
 
 var imagesArray = ["images/kitten0.jpg","images/kitten1.jpg","images/kitten2.jpg","images/kitten3.jpg","images/kitten4.jpg"]
 
+
+
 function displayImage(){
     var num = Math.floor(Math.random()*5);
-    document.images.src = imagesArray[num];
+    document.getElementById('kittyImg2').src= imagesArray[num];
 }
 
-var myButton = document.getElementById("mainButton");
 
-myButton.addEventListener("onClick",displayImage);
+var myButton = document.getElementById('mainButton');
+
+myButton.addEventListener("click",displayImage);
 
 displayImage();
