cd7000b46f02e6c397126dd581958d0358c12732
Andrew Hill <ahill7.ah@gmail.com>
2019-04-30 21:28:28 -0700

    class intsruction

--- Class 11/main.js
+++ Class 11/main.js
@@ -1,15 +1,17 @@
 var switchLogo = function(){
-    var imgCurrentSrc = document.getElementById('logo').scroll;
 
+    var imgCurrentSrc = document.getElementById('logo').src;
+    console.log(imgCurrentSrc)
     var src1 = 'images/pepsilogo.png';
     var src2 = 'images/pepsi2.png';
+    console.log(src2)
 
     if (imgCurrentSrc === src1){
-        imgCurrentSrc = src2
+        document.getElementById('logo').src = src2;
     }
     else{
-        imgCurrentSrc = src1;
+        document.getElementById('logo').src = src1;
     }
 }
 
-document.getElementById('switchLogo').addEventListener('click, switchLogo;')
+document.getElementById('logoSwitch').addEventListener('click', switchLogo);
