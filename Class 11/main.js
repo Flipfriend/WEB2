2140f606d1f9554c0ef432fa12008b56cb6ef6c1
Andrew Hill <ahill7.ah@gmail.com>
2019-05-02 16:19:19 -0700

    renamed the first js page and made a new one for the assignment. added a logo and a style sheet

--- Class 11/main.js
+++ Class 11/main.js
@@ -1,17 +1,2 @@
-var switchLogo = function(){
+var menu = document.getElementById('menuDrop');
 
-    var imgCurrentSrc = document.getElementById('logo').src;
-    console.log(imgCurrentSrc)
-    var src1 = 'images/pepsilogo.png';
-    var src2 = 'images/pepsi2.png';
-    console.log(src2)
-
-    if (imgCurrentSrc === src1){
-        document.getElementById('logo').src = src2;
-    }
-    else{
-        document.getElementById('logo').src = src1;
-    }
-}
-
-document.getElementById('logoSwitch').addEventListener('click', switchLogo);
