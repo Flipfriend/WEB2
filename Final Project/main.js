c45ef4fff5c30f51efb70362c97c7432edeb07a9
Andrew Hill <ahill7.ah@gmail.com>
2019-06-28 12:38:55 -0700

    removed the css for othbtn added jquery to change the checked input (not working)

--- Final Project/main.js
+++ Final Project/main.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
-    
+    // hide nav on load
     $('#navbar1').hide();
-    
+    // Menu toggle    
     $('#mnubtn').click(function(){
         $('#navbar1').toggle("slow");
         console.log('fire!')
     });    
-
+// thank you message at submit
     $('form').submit(function(event){
         event.preventDefault();
     })
+
+    $('ui-button > input').click(function(){
+        $(this).toggleClass('.ui-check', addOrRemove)
+    });
 });
